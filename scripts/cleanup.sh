162c0feaf9e2a673345269f4028fb5feef31db1c
ottontech <158478856+ottontech@users.noreply.github.com>
2024-02-01 15:56:09 +0100

    Create cleanup.sh

---
+++ scripts/cleanup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DOCKER_COMPOSE="docker-compose"
+
+if ! command -v docker-compose
+then
+        DOCKER_COMPOSE="docker compose"
+        echo "Docker compose command set to new style $DOCKER_COMPOSE"
+fi
+
+$DOCKER_COMPOSE down
+docker volume rm --force tak-server_db_data
+rm -rf tak
+rm -rf /tmp/takserver
+
+# Comment me out to save yourself rebuilding........
+docker image rm tak-server_db --force
+docker image rm tak-server_tak --force
